d23f05cc0d13dedf5577be8bb6c93fd8a60ba7fc
Tamim Jabr <tj222kg@student.lnu.se>
2022-05-12 19:11:30 +0200

    show icons in tab bar

--- App/config/Navigation.tsx
+++ App/config/Navigation.tsx
@@ -1,18 +1,37 @@
 import { NavigationContainer } from '@react-navigation/native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Home from '../screens/Home';
 import Settings from '../screens/Settings';
-
+import MotionSensorData from '../screens/MotionSensorData';
+import { Entypo } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import Camera from '../screens/Camera';
 
 const Tab = createBottomTabNavigator();
 
 
 const MyTabs = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={Home} />
+    <Tab.Navigator screenOptions={({ route }) => ({
+      tabBarIcon: ({ focused, color, size }) => {
+        let iconSize = focused ? 32 : 24
+
+        switch (route.name) {
+          case 'Camera':
+            return <Entypo name="camera" size={iconSize} color="black" />
+          case 'Settings':
+            return <Ionicons name="settings" size={iconSize} color="black" />
+          case 'Motion Sensor':
+            return <MaterialCommunityIcons name="motion-sensor" size={iconSize} color="black" />
+        }
+      },
+      tabBarShowLabel: false,
+    })
+    } >
+      <Tab.Screen name="Camera" component={Camera} />
+      <Tab.Screen name="Motion Sensor" component={MotionSensorData} />
       <Tab.Screen name="Settings" component={Settings} />
-    </Tab.Navigator>
+    </Tab.Navigator >
   );
 }
 
--- App/screens/Camera.tsx
+++ App/screens/Camera.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import { Text, View, Image, StyleSheet, Dimensions, Button, ScrollView, RefreshControl } from 'react-native'
+import { View, Image, StyleSheet, Dimensions, Button, ScrollView, RefreshControl } from 'react-native'
 import useFetch, { ResponseType } from '../hooks/useFetch'
+
 
 const screenHeight = Dimensions.get('window').height
 
@@ -8,31 +9,32 @@ const styles = StyleSheet.create({
   img: {
     width: '100%',
     height: screenHeight * 0.5,
+    justifyContent: 'center',
+
   },
 })
 
 
-const Home = () => {
-  const { data, getImage } = useFetch('http://192.168.0.14/cam-hi.jpg', ResponseType.BLOB)
-  const [refreshing, setRefreshing] = React.useState(false)
+const Camera = () => {
+  const { data, getImage, loading } = useFetch('http://192.168.0.14/cam-high.jpg', ResponseType.BLOB)
+
 
   const onRefresh = async () => {
     await getImage()
   }
 
-
   return (
     <ScrollView refreshControl={
       <RefreshControl
-        refreshing={refreshing}
+        refreshing={loading}
         onRefresh={onRefresh}
       />
     } >
-      <Text>Home</Text>
+      {loading && <View style={[styles.img, { backgroundColor: '#D1D1D1' }]}></View>}
       {data && <Image source={{ uri: data }} style={styles.img} resizeMode='contain' />}
       <Button title='Refresh' onPress={onRefresh} />
     </ScrollView>
   )
 }
 
-export default Home
+export default Camera
