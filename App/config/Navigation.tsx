105f38c2623693abc8d1d56cc71b0e8a3d7fe925
Tamim Jabr <tj222kg@student.lnu.se>
2022-05-12 19:35:40 +0200

    style tabbar

--- App/config/Navigation.tsx
+++ App/config/Navigation.tsx
@@ -6,26 +6,43 @@ import { Entypo } from '@expo/vector-icons';
 import { Ionicons } from '@expo/vector-icons';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import Camera from '../screens/Camera';
+import colors from '../constants/colors';
+import { StatusBar } from 'react-native';
 
 const Tab = createBottomTabNavigator();
 
 
 const MyTabs = () => {
   return (
-    <Tab.Navigator screenOptions={({ route }) => ({
+    <Tab.Navigator sceneContainerStyle={{
+      backgroundColor: colors.dark
+    }} screenOptions={({ route }) => ({
       tabBarIcon: ({ focused, color, size }) => {
         let iconSize = focused ? 32 : 24
+        let iconColor = focused ? colors.blue : colors.gray
 
         switch (route.name) {
           case 'Camera':
-            return <Entypo name="camera" size={iconSize} color="black" />
+            return <Entypo name="camera" size={iconSize} color={iconColor} />
           case 'Settings':
-            return <Ionicons name="settings" size={iconSize} color="black" />
+            return <Ionicons name="settings" size={iconSize} color={iconColor} />
           case 'Motion Sensor':
-            return <MaterialCommunityIcons name="motion-sensor" size={iconSize} color="black" />
+            return <MaterialCommunityIcons name="motion-sensor" size={iconSize} color={iconColor} />
         }
       },
       tabBarShowLabel: false,
+      tabBarStyle: {
+        backgroundColor: colors.dark,
+        borderTopColor: 'transparent',
+      },
+      headerTitleAlign: 'center',
+      headerStyle: {
+        backgroundColor: colors.dark,
+
+      },
+      headerTitleStyle: {
+        color: colors.gray,
+      }
     })
     } >
       <Tab.Screen name="Camera" component={Camera} />
@@ -38,6 +55,7 @@ const MyTabs = () => {
 export default () => {
   return (
     <NavigationContainer>
+      <StatusBar backgroundColor={colors.dark} barStyle='light-content' />
       <MyTabs />
     </NavigationContainer>
   )
---
+++ App/constants/colors.ts
@@ -0,0 +1,5 @@
+export default {
+  blue: '#00A0D6',
+  gray: '#838383',
+  dark: '#212121'
+}
