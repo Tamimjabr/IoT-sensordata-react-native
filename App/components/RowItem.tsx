b39fb58cc142698754b308378100fc4b044e4cee
Tamim Jabr <tj222kg@student.lnu.se>
2022-05-13 12:24:09 +0200

    add settings list

---
+++ App/components/RowItem.tsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import { Text, TouchableOpacity, StyleSheet, View } from 'react-native'
+import colors from '../constants/colors'
+
+const styles = StyleSheet.create({
+  row: {
+    marginVertical: 10,
+    paddingVertical: 16,
+    paddingHorizontal: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  text: {
+    fontSize: 16,
+    color: colors.white
+  },
+  divider: {
+    backgroundColor: colors.white,
+    marginHorizontal: 20,
+    height: StyleSheet.hairlineWidth
+  }
+})
+
+export const RowItem = ({ title, rightIcon, onPress }: { title: string, rightIcon: React.ReactElement, onPress: () => void }) => {
+  return (
+    <TouchableOpacity style={styles.row} onPress={onPress}>
+      <Text style={styles.text}>{title}</Text>
+      {rightIcon}
+    </TouchableOpacity>
+  )
+}
+
+export const RowDivider = () => {
+  return <View style={styles.divider} />
+}
--- App/screens/Settings.tsx
+++ App/screens/Settings.tsx
@@ -1,11 +1,30 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, SafeAreaView, FlatList, StyleSheet, StatusBar } from 'react-native'
+import { RowDivider, RowItem } from '../components/RowItem';
+import { Entypo } from '@expo/vector-icons';
+import colors from '../constants/colors';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+});
+
+const settings = ['Image Quality', 'About'];
 
 const Settings = () => {
   return (
-    <View>
-      <Text>Settings</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={settings}
+        renderItem={({ item }) => (
+          <RowItem title={item} onPress={() => console.log('pressed')} rightIcon={<Entypo name="chevron-small-right" size={24} color={colors.white} />} />
+        )}
+        keyExtractor={(item) => item}
+        ItemSeparatorComponent={() => <RowDivider />}
+      />
+    </SafeAreaView>
   )
 }
 
