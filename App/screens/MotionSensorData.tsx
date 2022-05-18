9a88bcd6a9c3c6882e256655071fa48f89668851
Tamim Jabr <tj222kg@student.lnu.se>
2022-05-18 22:52:20 +0200

    get data from motion sensor

--- App/screens/MotionSensorData.tsx
+++ App/screens/MotionSensorData.tsx
@@ -1,9 +1,27 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { FlatList, Text } from 'react-native'
+import { RowDivider, RowItem } from '../components/RowItem'
+import useFetch from '../hooks/useFetch'
+
+
+
+
 
 const MotionSensorData = () => {
+  // todo fetch from own server
+  const { data, getData, loading } = useFetch('http://192.168.0.7:5051/api/v1/sensors')
+  console.log('data:', data)
+
   return (
-    <Text>MotionSensorData</Text>
+    <FlatList
+      data={data ? data.data : []}
+      renderItem={({ item }) => (
+        <RowItem title={`Motion ${item._time}`} onPress={() => undefined} />
+      )
+      }
+      keyExtractor={(item, index) => index.toString()}
+      ItemSeparatorComponent={() => <RowDivider />}
+    />
   )
 }
 
