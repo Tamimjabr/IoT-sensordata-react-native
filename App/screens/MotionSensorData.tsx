e5797c1629bd7bdd5ec9f8163ca6a260f0cc6946
Tamim Jabr <tj222kg@student.lnu.se>
2022-05-18 23:07:50 +0200

    handle no existing data for the sensors

--- App/screens/MotionSensorData.tsx
+++ App/screens/MotionSensorData.tsx
@@ -1,8 +1,23 @@
 import React from 'react'
-import { FlatList, RefreshControl } from 'react-native'
+import { FlatList, RefreshControl, ScrollView, StyleSheet, Text } from 'react-native'
 import { RowDivider, RowItem } from '../components/RowItem'
+import colors from '../constants/colors'
 import useFetch from '../hooks/useFetch'
 
+const styles = StyleSheet.create({
+  text: {
+    backgroundColor: colors.white,
+    color: colors.blue,
+    textAlign: 'center',
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginVertical: 10,
+    borderRadius: 10,
+    padding: 10,
+    width: '50%',
+    alignSelf: 'center'
+  }
+})
 
 const MotionSensorData = () => {
   // todo fetch from own server
@@ -13,19 +28,29 @@ const MotionSensorData = () => {
   }
 
   return (
-    <FlatList
-      refreshControl={<RefreshControl
-        refreshing={loading}
-        onRefresh={onRefresh}
-      />}
-      data={data ? data.data : []}
-      renderItem={({ item }) => (
-        <RowItem title={`Motion ${item._time}`} onPress={() => undefined} />
-      )
+    <>
+      {data && data.data.length === 0 ? <ScrollView
+        refreshControl={<RefreshControl
+          refreshing={loading}
+          onRefresh={onRefresh}
+        />}>
+        <Text style={styles.text}>No data</Text>
+      </ScrollView> :
+        <FlatList
+          refreshControl={<RefreshControl
+            refreshing={loading}
+            onRefresh={onRefresh}
+          />}
+          data={data ? data.data : []}
+          renderItem={({ item }) => (
+            <RowItem title={`Motion ${item._time}`} onPress={() => undefined} />
+          )
+          }
+          keyExtractor={(item, index) => index.toString()}
+          ItemSeparatorComponent={() => <RowDivider />}
+        />
       }
-      keyExtractor={(item, index) => index.toString()}
-      ItemSeparatorComponent={() => <RowDivider />}
-    />
+    </>
   )
 }
 
