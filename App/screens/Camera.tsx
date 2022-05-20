7a7184ee78ca15594e95b1605304622162a565a0
Tamim Jabr <tj222kg@student.lnu.se>
2022-05-20 15:02:52 +0200

    enable user to upload img to cloudinary

--- App/screens/Camera.tsx
+++ App/screens/Camera.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
-import { View, Image, StyleSheet, Dimensions, ScrollView, RefreshControl, Text } from 'react-native'
+import { View, Image, StyleSheet, Dimensions, ScrollView, RefreshControl, Text, Alert } from 'react-native'
 import { GlobalStateContext } from '../global-state/GlobalState'
 import { FontAwesome } from '@expo/vector-icons';
 import useFetchBlob from '../hooks/useFetchBlob'
 import Button from '../components/Button'
 import colors from '../constants/colors'
+
+
+let CLOUDINARY_URL = 'https://api.cloudinary.com/v1_1/da1wrhmsi/auto/upload'
 
 const screenHeight = Dimensions.get('window').height
 
@@ -36,9 +39,30 @@ const Camera = () => {
   const { imgResolution } = React.useContext(GlobalStateContext)
   const { data, getImage, loading } = useFetchBlob(`http://192.168.0.14/cam-${imgResolution}.jpg`)
 
-  console.log('imgResolution', imgResolution)
   const onRefresh = async () => {
     await getImage(`http://192.168.0.14/cam-${imgResolution}.jpg`)
+  }
+
+  const uploadImage = async () => {
+    try {
+      if (data) {
+        const formData = new FormData();
+        let base64Img = `data:image/jpg;base64,${data}`;
+        formData.append("file", base64Img);
+        formData.append("upload_preset", "esp32-camera");
+        const response = await fetch(CLOUDINARY_URL, {
+          method: 'POST',
+          body: formData
+        })
+
+        if (!response.ok) {
+          throw new Error('Upload failed')
+        }
+        Alert.alert('Success', 'Image uploaded successfully')
+      }
+    } catch (error: any) {
+      Alert.alert('Error', `Unable to upload image.\n ${error.message}`)
+    }
   }
 
   return (
@@ -54,7 +78,7 @@ const Camera = () => {
         <Image source={{ uri: data }} style={styles.img} resizeMode='contain' />
         <View style={styles.buttonContainer}>
           <Button text='Refresh' onPress={onRefresh} buttonIcon={(<FontAwesome name="refresh" size={24} color={colors.white} />)} />
-          <Button text='Upload' onPress={onRefresh} buttonIcon={(<FontAwesome name="cloud-upload" size={24} color={colors.white} />)} />
+          <Button text='Upload' onPress={uploadImage} buttonIcon={(<FontAwesome name="cloud-upload" size={24} color={colors.white} />)} />
         </View>
       </>)}
 
